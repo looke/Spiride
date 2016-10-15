9c36fb022c2a6abc5f84d29de83d4210f164a224
looke <52668057@qq.com>
2016-10-15 09:41:07 +0800

    Add Z axis

--- Arduino/ADXL345_SPI_Test/ADXL345_SPI_Test.ino
+++ Arduino/ADXL345_SPI_Test/ADXL345_SPI_Test.ino
@@ -7,7 +7,7 @@ const int AccSelectPin = 9;
 void setup() 
 {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+  Serial.begin(115200);
   //SPI读取初始化
   pinMode(AccSelectPin, OUTPUT);
   //pinMode(GyroSelectPin, OUTPUT);
@@ -26,9 +26,11 @@ void setup()
 void loop() 
 {
   // put your main code here, to run repeatedly:
-  delay(100);
-  //readAccRegValue(READCommand_DATA_FORMAT);
-  readAccXValue();
+  delay(10);
+  //readAccRegValue(READCommand_DATA_INT_ENABLE);
+  //readAccRegValue(READCommand_OFFSET_Y);
+  //readAccRegValue(READCommand_OFFSET_Z);
+  readAccXYZValue();
 }
 
 void setupACC()
@@ -40,7 +42,7 @@ void setupACC()
 
   digitalWrite(AccSelectPin, LOW);
   SPI.transfer(WRITECommand_DATA_FORMAT);
-  SPI.transfer(0x01); //4g range
+  SPI.transfer(0x03); //16g range
   digitalWrite(AccSelectPin, HIGH);
   //Serial.println(reg,BIN);
 }
@@ -71,4 +73,70 @@ void readAccXValue()
   Serial.print(x);
   Serial.print("\t");
   Serial.println(degree);
+}
+
+void readAccZValue()
+{
+  digitalWrite(AccSelectPin, LOW);
+  SPI.transfer(READCommand_DATA_Z0);
+  byte zLSB = SPI.transfer(0x00);
+  byte zMSB = SPI.transfer(0x00);
+  digitalWrite(AccSelectPin, HIGH);
+  int z = ((zMSB << 8) | zLSB);
+  //x=x+5;
+  //double degree = (double)z/161;
+  //degree = asin(degree)*180/PI;
+  //Serial.println(xLSB,BIN);
+  //Serial.println(xMSB,BIN);
+  //Serial.println(x,BIN);
+  Serial.print(zMSB,BIN);
+  Serial.print("\t");
+  Serial.println(zLSB,BIN);
+  //Serial.print("\t");
+  //Serial.println(degree);
+}
+
+void readAccXYZValue()
+{
+  digitalWrite(AccSelectPin, LOW);
+  SPI.transfer(READCommand_DATA_X0);
+  byte xLSB = SPI.transfer(0x00);
+  byte xMSB = SPI.transfer(0x00);
+  //digitalWrite(AccSelectPin, HIGH);
+
+  //digitalWrite(AccSelectPin, LOW);
+  //SPI.transfer(READCommand_DATA_Y0);
+  byte yLSB = SPI.transfer(0x00);
+  byte yMSB = SPI.transfer(0x00);
+  //digitalWrite(AccSelectPin, HIGH);
+
+  //digitalWrite(AccSelectPin, LOW);
+  //SPI.transfer(READCommand_DATA_Z0);
+  byte zLSB = SPI.transfer(0x00);
+  byte zMSB = SPI.transfer(0x00);
+  digitalWrite(AccSelectPin, HIGH);
+  
+  
+  int x = ((xMSB << 8) | xLSB);
+  int y = ((yMSB << 8) | yLSB);
+  int z = ((zMSB << 8) | zLSB);
+  
+  //x=x+5;
+  
+  //double degree = (double)x/161;
+  //degree = asin(degree)*180/PI;
+  //Serial.println(xLSB,BIN);
+  //Serial.println(xMSB,BIN);
+  //Serial.println(x,BIN);
+  Serial.print("X:");
+  Serial.print(x);
+  Serial.print("\t");
+  Serial.print("Y:");
+  Serial.print(y);
+  Serial.print("\t");
+  Serial.print("Z:");
+  Serial.println(z);
+  
+  
+  //Serial.println(degree);
 }
--- Arduino/libraries/ADXL345/ADXL345_Command.h
+++ Arduino/libraries/ADXL345/ADXL345_Command.h
@@ -1,11 +1,29 @@
-const byte READCommand_DEVID =   0x80;
+const byte READCommand_DEVID = 0xC0;
+
 const byte READCommand_BW_RATE = 0xEC;
+
+const byte READCommand_OFFSET_X = 0xDE;
+const byte READCommand_OFFSET_Y = 0xDF;
+const byte READCommand_OFFSET_Z = 0xE0;
+
+const byte READCommand_DATA_ACT_CTRL = 0xE7;
+//const byte WRITECommand_DATA_ACT_CTRL = 0x71;
+const byte READCommand_DATA_TAP_AXES = 0xEA;
+const byte READCommand_DATA_TAP_STATUS = 0xEB;
 
 const byte READCommand_POWER_CTL = 0xED;
 const byte WRITECommand_POWER_CTL = 0x6D;
+
+const byte READCommand_DATA_INT_ENABLE = 0xEE;
 
 const byte READCommand_DATA_FORMAT = 0xF1;
 const byte WRITECommand_DATA_FORMAT = 0x71;
 
 const byte READCommand_DATA_X0 = 0xF2;
 const byte READCommand_DATA_X1 = 0xF3;
+
+const byte READCommand_DATA_Y0 = 0xF4;
+const byte READCommand_DATA_Y1 = 0xF5;
+
+const byte READCommand_DATA_Z0 = 0xF6;
+const byte READCommand_DATA_Z1 = 0xF7;
